e646aedfd094deae5935b17a95f50fb2300b1965
hironomiu <hironomiu@gmail.com>
2021-01-29 14:12:00 +0900

    commited

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import { render , screen } from '@testing-library/react'
-import App from './components/App'
+import React from "react"
+import { render, screen } from "@testing-library/react"
+import App from "./components/App"
 
-describe('Rendering', () => {
-    it("Should render all elements correctly", () => {
-        render(<App />)
-        expect(screen.getByRole("textbox")).toBeTruthy()
-        expect(screen.getAllByRole("button")[0]).toBeTruthy()
-        expect(screen.getAllByRole("button")[1]).toBeTruthy()
-    })
-});
+describe("Rendering", () => {
+  it("Should render all elements correctly", () => {
+    render(<App />)
+    expect(screen.getByRole("textbox")).toBeTruthy()
+    expect(screen.getAllByRole("button")[0]).toBeTruthy()
+    expect(screen.getAllByRole("button")[1]).toBeTruthy()
+  })
+})
--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState } from "react"
 
 const App = (props) => {
   const [count, setCount] = useState(props.count)
-  const [message,setMessage] = useState(props.message)
+  const [message, setMessage] = useState(props.message)
 
   return (
     <>
-      <p> {message}：{count} </p>
-      <button onClick={() => setCount(cnt => cnt + 1)}> + </button>
-      <button onClick={() => setCount(cnt => cnt - 1)}> - </button>
-      <div>message : <input type="text" value={message} onChange={e => setMessage(msg => msg = e.target.value)}/></div>
+      <p>
+        {" "}
+        {message}：{count}{" "}
+      </p>
+      <button onClick={() => setCount((cnt) => cnt + 1)}> + </button>
+      <button onClick={() => setCount((cnt) => cnt - 1)}> - </button>
+      <div>
+        message :{" "}
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage((msg) => (msg = e.target.value))}
+        />
+      </div>
     </>
   )
 }
 App.defaultProps = {
-  message: 'hello',
-  count: 0
+  message: "hello",
+  count: 0,
 }
 
 export default App
