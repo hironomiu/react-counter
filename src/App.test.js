80309f4f150b1149d032573d3df9ed1fc96d96ad
hironomiu <hironomiu@gmail.com>
2021-02-01 16:11:50 +0900

    fixed #3

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,9 @@
 import React from "react"
-import { render, screen } from "@testing-library/react"
+import { render, screen, cleanup } from "@testing-library/react"
 import App from "./components/App"
+import userEvent from "@testing-library/user-event"
+
+afterEach(() => cleanup())
 
 describe("Rendering", () => {
   it("Should render all elements correctly", () => {
@@ -8,5 +11,19 @@ describe("Rendering", () => {
     expect(screen.getByRole("textbox")).toBeTruthy()
     expect(screen.getAllByRole("button")[0]).toBeTruthy()
     expect(screen.getAllByRole("button")[1]).toBeTruthy()
+    const inputValue = screen.getByRole("textbox")
+    expect(inputValue.value).toBe("hello")
+  })
+})
+
+describe("Input form onChange event", () => {
+  it("Shold ", () => {
+    render(<App />)
+    const inputValue = screen.getByRole("textbox")
+    new Promise(() => {
+      userEvent.type(inputValue, "test")
+    }).then(() => {
+      expect(inputValue.value).toBe("test")
+    })
   })
 })
